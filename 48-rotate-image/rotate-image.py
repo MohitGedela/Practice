29c3d7f6e6c31c063bf941be634153c2504b50db
Mohit Gedela <62566794+MohitGedela@users.noreply.github.com>
2026-08-17 15:47:25 -0400

    Time: 0 ms (100%) | Memory: 19.2 MB (68.33%) - LeetSync
    Removed docstring comment about in-place modification.

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -1,8 +1,6 @@
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
-        """
-        Do not return anything, modify matrix in-place instead.
-        """
+
         for i in range(len(matrix)):
             for j in range(i+1, len(matrix)):
                 temp = matrix[i][j]
